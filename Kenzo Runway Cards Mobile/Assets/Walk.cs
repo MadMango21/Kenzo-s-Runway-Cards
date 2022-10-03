3cd6f1ece2eefac88e04de04965a9f6fde3b43b6
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-10-03 09:29:29 +0100

    Start walk script

---
+++ Kenzo Runway Cards Mobile/Assets/Walk.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    CharacterController characterController;
+    
+    public float speed = 6.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
