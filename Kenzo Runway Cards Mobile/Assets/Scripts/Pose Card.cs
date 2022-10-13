ac08cd7499d1959f12718a506fcc535b35264024
Humza Mustafa <107266383+MadMango21@users.noreply.github.com>
2022-10-13 11:42:20 +0100

    level map1 proto in

--- Kenzo Runway Cards Mobile/Assets/Scripts/Pose Card.cs
+++ Kenzo Runway Cards Mobile/Assets/Scripts/Pose Card.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TouchCards : MonoBehaviour
+public class PoseCard : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
